5116b563a677c822c5d3aa6dbf7c600d3ada101e
EfrainSavasta <133943692+EfrainSavasta@users.noreply.github.com>
2023-06-19 12:20:56 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,23 @@
 let express = require('express');
 let app = express();
 
+let absolutePath = __dirname + "/views/index.html";
+
+
+
+app.use(express.static(__dirname + "/public"))
+
+//app.get("/", (req, res) => {
+ // res.send("Hello Express")
+//})
+
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath)
+})
+
+console.log("Hello World");
+
+
 
 
 
